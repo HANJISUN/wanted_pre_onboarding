dfa5572852ab1276062d1162a6973b279fbb1548
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-04-21 23:27:14 +0900

    fix: typo
    Prettier 설정 변경 후 자동으로 수정 된 부분

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import './App.css';
-import Toggle from './components/Toggle';
-import Tab from './components/Tab';
+import "./App.css";
+import Toggle from "./components/Toggle";
+import Tab from "./components/Tab";
+import Slider from "./components/Slider";
 
 function App() {
   return (
@@ -8,7 +9,10 @@ function App() {
       <h1>원티드 프리온보딩 프론트엔드 선발 과제</h1>
       <br />
       <Toggle />
+      <br />
       <Tab />
+      <br />
+      <Slider />
     </div>
   );
 }
